53e132787ddb579a4603d2cd1ba851b2b79162e6
Masterchef365 <duncan.freeman1@gmail.com>
2016-05-26 15:30:37 -0700

    Finished!

---
+++ Interview/Interview/Histo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    public class Histo
+    {
+        List<int> data; //Stored histogram
+        List<int> buckets;
+        public Histo (List<int> data)
+        {
+            buckets = new List<int> { 0,0,0,0,0,0,0,0,0,0 };
+            this.data = data;
+            foreach (int i in data) {
+                for (int age = 0; age < 10; age++)
+                {
+                    if (i >= age * 10 && i <= age * 10 + 9)
+                    {
+                        buckets[age] += 1;
+                    }
+                }
+            }
+        }
+
+        public List<int> get_histo ()
+        {
+            return buckets;
+        }
+
+    }
+}
---
+++ Interview/Interview/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interview Program, \n Histogram:");
+            Histo histo = new Histo(new List<int> { 99, 99, 50, 50, 1, 1 });
+            List<int> histogram = histo.get_histo();
+            foreach (int i in histogram)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.Read(); //Wait for user to type
+        }
+    }
+}
